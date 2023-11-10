4a9402316c28171edd87b8a81ea7003361fefee5
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-11-10 10:13:09 -0500

    dsf

---
+++ bon-appe/js/cards.js
@@ -0,0 +1,1 @@
+const card = ["F18", "jet"];
--- bon-appe/js/main.js
+++ bon-appe/js/main.js
@@ -1,7 +1,14 @@
-import '../styles/style.css'
-import javascriptLogo from './javascript.svg'
+import "../styles/style.css"
+import {card} from "./cards";
+
+
+
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+
+
+
+
 
 document.querySelector('#app').innerHTML = `
   <div>
