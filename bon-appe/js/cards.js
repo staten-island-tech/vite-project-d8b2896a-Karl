d0e25793ddf47080351acd38369f531bdad9c60e
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-11-17 10:15:15 -0500

    fdf

---
+++ bon-appe/js/cards.js
@@ -0,0 +1,218 @@
+const cards = [
+    {
+        name: "A-10 warthog",
+        division: "air force",
+        speed: "fast",
+        usage: ["gunner","support"],
+        image: "../A-10-warthog.jpg",
+
+    },
+    {
+        name: "AAV",
+        division: "navy",
+        speed: "medium",
+        usage: ["transport","support"],
+        image: "../AAV-amphibious-craft.jpg",
+
+    },
+    {
+        name: "abram tank",
+        division: "army",
+        speed: "medium",
+        usage: ["artillery","combat"],
+        image: "../abram-tank.jpg",
+
+    },
+    {
+        name: "anzac class frigate",
+        division: "navy",
+        speed: "medium",
+        usage: ["defense","support"],
+        image: "../anzac-class-frigate.jpg",
+
+    },
+    {
+        name: "B2 Bomber",
+        division: "air force",
+        speed: "fast",
+        usage: ["Artillery"," Bomber"],
+        image: "../B2-Bomber.jpg",
+
+    },
+    {
+        name: "cyclone class patrol boat",
+        division: "navy",
+        speed: "medium",
+        usage: ["security","defense"],
+        image: "../cyclone-class-patrol.jpg",
+
+    },
+    {
+        name: "F16 fighter jet",
+        division: "air force",
+        speed: "fast",
+        usage: ["combat",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+    {
+        name: "",
+        division: "",
+        speed: "",
+        usage: ["",""],
+        image: "",
+
+    },
+]
