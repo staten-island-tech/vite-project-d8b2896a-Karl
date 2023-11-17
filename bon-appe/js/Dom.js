e38dd2fb9cbaa53249f24de49cabbe9b6633d4b4
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-11-17 09:47:17 -0500

    nn

--- bon-appe/js/Dom.js
+++ bon-appe/js/Dom.js
@@ -1,3 +1,8 @@
 const DOMSelectors = {
-    
+    navy: document.querySelector("#navy-button"),
+    army: document.querySelector("#army-button"),
+    air: document.querySelector("#air-button"),
+    navy: document.querySelector("#navy-button"),
 }
+
+export {DOMSelectors};
